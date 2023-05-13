bf3b0e42f40fc49bc4a58868057592aa86cc3e9d
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-05-13 19:52:18 +0200

    feat : added annotation

--- src/main/java/fr/momozer/test/alten/shopBackend/shopBackend/repository/ProductRepository.java
+++ src/main/java/fr/momozer/test/alten/shopBackend/shopBackend/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package fr.momozer.test.alten.shopBackend.shopBackend.repository;
 
 import fr.momozer.test.alten.shopBackend.shopBackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
---
+++ src/main/java/fr/momozer/test/alten/shopBackend/shopBackend/service/serviceImpl/ProductServiceImpl.java
@@ -0,0 +1,9 @@
+package fr.momozer.test.alten.shopBackend.shopBackend.service.serviceImpl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ProductServiceImpl {
+}
