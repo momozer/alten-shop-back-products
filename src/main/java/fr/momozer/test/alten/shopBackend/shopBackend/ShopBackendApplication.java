232af5525dfdc9a9c1542e20f31d5cf106308f41
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-05-13 18:46:59 +0200

    feat : first commit + creation of the project

---
+++ src/main/java/fr/momozer/test/alten/shopBackend/shopBackend/ShopBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.momozer.test.alten.shopBackend.shopBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/momozer/test/alten/shopBackend/shopBackend/ShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.momozer.test.alten.shopBackend.shopBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
